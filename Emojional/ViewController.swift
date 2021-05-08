821e01100c5483abce5e26781ec5a4362e288305
LianaAdaza <liana.adaza@gmail.com>
2021-05-08 00:55:06 -0400

    Final Iteration

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    let emojis = ["🤕": "troubled", "😚": "cheery"]
+   
+    let customMessages = ["troubled": ["It'll all be better!", "Hang in there!", "You're not alone, please take care!"], "cheery": ["You're so lovely!", "You're doing great!", "Spread love!"]]
+    
+    @IBAction func showMessage(sender: UIButton) {
 
+        let selectedEmotion = (sender.titleLabel?.text)!
+
+        let number = Int.random(in: 0..<3)
+        let options = customMessages[emojis[selectedEmotion]!]!
+        let emojiMessage = options[number]
+
+        let alertController = UIAlertController(title: "🚨 INCOMING MESSAGE 🚨", message: emojiMessage, preferredStyle: UIAlertController.Style.alert)
+
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+
+            present(alertController, animated: true, completion: nil)
+    }
 }
-
